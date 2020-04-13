663e25a829caa22be127d8203eafd4a50a962c87
Pranay <pranay@Pranays-MacBook-Pro.local>
2020-04-13 13:51:19 -0400

    disable copy from website

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -1,9 +1,12 @@
-// $(document).ready(function () {
-//     $(document).click(function (event) {
-//         var clickover = $(event.target);
-//         var _opened = $(".navbar-collapse").hasClass("show");
-//         if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-//             $("button.navbar-toggler").click();
-//         }
-//     });
-// });
+$(document).ready(function () {
+    // $(document).click(function (event) {
+    //     var clickover = $(event.target);
+    //     var _opened = $(".navbar-collapse").hasClass("show");
+    //     if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+    //         $("button.navbar-toggler").click();
+    //     }
+    // });
+    $('body').bind('copy paste cut drag drop', function (e) {
+        e.preventDefault();
+    });
+});
